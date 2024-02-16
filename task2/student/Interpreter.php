1aa2df9006389d098a7f7bc592a2541ed9331c73
Martan03 <martinslezak03@gmail.com>
2024-02-16 13:34:27 +0100

    Add read and move opcode exe to Interpreter file

--- task2/student/Interpreter.php
+++ task2/student/Interpreter.php
@@ -5,6 +5,7 @@ namespace IPP\Student;
 use DOMText;
 use IPP\Core\AbstractInterpreter;
 use IPP\Core\Exception\NotImplementedException;
+use IPP\Core\Exception\ParameterException;
 use IPP\Core\Exception\XMLException;
 use IPP\Core\Settings;
 
@@ -26,7 +27,8 @@ class Interpreter extends AbstractInterpreter
 
         ksort($this->instructions);
         foreach ($this->instructions as $inst) {
-            $inst->execute($this->storage);
+            $this->execInstruction($inst);
+            //$inst->execute($this->input, $this->storage);
         }
 
         echo var_dump($this->storage);
@@ -63,5 +65,197 @@ class Interpreter extends AbstractInterpreter
         }
 
         return $instructions;
+    }
+
+    private function execInstruction(Instruction $inst) {
+        match ($inst->opcode) {
+            "MOVE" => $this->move($inst),
+            "CREATEFRAME" => $this->storage->create(),
+            "PUSHFRAME" => $this->storage->push(),
+            "POPFRAME" => $this->storage->pop(),
+            "DEFVAR" => $this->defVar($inst),
+            "CALL" => $this->none(),
+            "RETURN" => $this->none(),
+            "PUSHS" => $this->none(),
+            "POPS" => $this->none(),
+            "ADD", "SUB", "MUL", "IDIV" => $this->calc($inst),
+            "LT" => $this->cmp($inst, "lt"),
+            "GT" => $this->gt($inst, "gt"),
+            "EQ" => $this->eq($inst, "eq"),
+            "AND" => $this->and($inst),
+            "OR" => $this->or($inst),
+            "NOT" => $this->not($inst),
+            "INT2CHAR" => $this->int2char($inst),
+            "STRI2INT" => $this->stri2int($inst),
+            "READ" => $this->read($inst),
+            "WRITE" => $this->none(),
+            "CONCAT" => $this->none(),
+            "STRLEN" => $this->none(),
+            "GETCHAR" => $this->none(),
+            "SETCHAR" => $this->none(),
+            "TYPE" => $this->none(),
+            "LABEL" => $this->none(),
+            "JUMP" => $this->none(),
+            "JUMPIFEQ" => $this->none(),
+            "JUMPIFNEQ" => $this->none(),
+            "EXIT" => $this->none(),
+            "DPRINT" => $this->none(),
+            "BREAK" => $this->none(),
+            default => $this->none(),
+        };
+    }
+
+    private function none() {}
+
+    private function move(Instruction $inst) {
+        $item = $this->getSymb($inst->args[1]);
+
+        list($frame, $name) = explode('@', $inst->args[0]->getValue());
+        $this->storage
+            ->add($frame, $name, $item->getType(), $item->getValue());
+    }
+
+    private function defVar(Instruction $inst) {
+        list($frame, $name) = explode('@', $inst->args[0]->getValue());
+        // TODO: use correct exception
+        if (!$this->storage->defVar($frame, $name))
+            throw new ParameterException();
+    }
+
+    private function calc(Instruction $inst) {
+        $item1 = $this->getSymb($inst->args[1]);
+        // TODO: use correct exception
+        if ($item1->getType() != 'int')
+            throw new ParameterException();
+
+        $item2 = $this->getSymb($inst->args[2]);
+        // TODO: use correct exception
+        if ($item2->getType() != 'int')
+            throw new ParameterException();
+
+        $res = match ($inst->opcode) {
+            "ADD" => (int)$item1->getValue() + (int)$item2->getValue(),
+            "SUB" => (int)$item1->getValue() - (int)$item2->getValue(),
+            "MUL" => (int)$item1->getValue() * (int)$item2->getValue(),
+            "IDIV" => (int)$item1->getValue() / (int)$item2->getValue(),
+        };
+
+        list($frame, $name) = explode('@', $inst->args[0]->getValue());
+        $this->storage->add($frame, $name, "int", $res);
+    }
+
+    private function cmp(Instruction $inst, callable $cmpFun) {
+        $item1 = $this->getSymb($inst->args[1]);
+        $item2 = $this->getSymb($inst->args[2]);
+        // TODO return code
+        if ($item1->getType() != $item2->getType())
+            return;
+
+        $res = call_user_func_array(
+            array($this, $cmpFun),
+            array($item1->getValue(), $item2->getValue())
+        );
+        list($frame, $name) = explode('@', $inst->args[0]->getValue());
+        $this->storage->add($frame, $name, "bool", $res);
+    }
+
+    private function and(Instruction $inst) {
+        $item1 = $this->getSymb($inst->args[1]);
+        $item2 = $this->getSymb($inst->args[2]);
+        // TODO return code
+        if ($item1->getType() != "bool" || $item2->getType() != "bool")
+            return;
+
+        $res = $item1->getValue() && $item2->getValue();
+        list($frame, $name) = explode('@', $inst->args[0]->getValue());
+        $this->storage->add($frame, $name, "bool", $res);
+    }
+
+    private function or(Instruction $inst) {
+        $item1 = $this->getSymb($inst->args[1]);
+        $item2 = $this->getSymb($inst->args[2]);
+        // TODO return code
+        if ($item1->getType() != "bool" || $item2->getType() != "bool")
+            return;
+
+        $res = $item1->getValue() || $item2->getValue();
+        list($frame, $name) = explode('@', $inst->args[0]->getValue());
+        $this->storage->add($frame, $name, "bool", $res);
+    }
+
+    private function not(Instruction $inst) {
+        $item = $this->getSymb($inst->args[1]);
+        if ($item->getType() != "bool")
+            return;
+
+        list($frame, $name) = explode('@', $inst->args[0]->getValue());
+        $this->storage->add($frame, $name, "bool", !$item->getValue());
+    }
+
+    private function int2char(Instruction $inst) {
+        $item = $this->getSymb($inst->args[1]);
+        if ($item->getType() != "int")
+            return;
+
+        list($frame, $name) = explode('@', $inst->args[0]->getValue());
+        $this->storage->add($frame, $name, "string", chr($item->getValue()));
+    }
+
+    private function stri2int(Instruction $inst) {
+        $item1 = $this->getSymb($inst->args[1]);
+        $item2 = $this->getSymb($inst->args[2]);
+        if ($item1->getType() != "string" || $item2->getType() != "int")
+            return;
+
+        $res = ord($item1->getValue()[$item2->getValue()]);
+        list($frame, $name) = explode('@', $inst->args[0]->getValue());
+        $this->storage->add($frame, $name, "int", $res);
+    }
+
+    private function read(Instruction $inst) {
+        $item = $this->getSymb($inst->args[1]);
+        $value = match ($item->getValue()) {
+            "int" => $this->input->readInt(),
+            "string" => $this->input->readString(),
+            "bool" => $this->input->readBool(),
+        };
+
+        $type = $value ? $item->getValue() : "nil";
+        $res = $value ?? null;
+        list($frame, $name) = explode('@', $inst->args[0]->getValue());
+        $this->storage->add($frame, $name, $type, $res);
+    }
+
+
+    private function getSymb(Arg $arg): StorageItem {
+        $type = $arg->getType();
+        $value = $arg->getValue();
+        $item = new StorageItem($type, $value);
+        if ($type == "var") {
+            list($fr, $n) = explode('@', $value);
+            $item = $this->storage->get($fr, $n);
+            $type = $item->getType();
+            $value = $item->getValue();
+        }
+
+        return $item;
+    }
+
+    private function save(string $var, ?string $type, mixed $value) {
+        list($frame, $name) = explode('@', $var);
+        $this->storage->add($frame, $name, $type, $value);
+    }
+
+
+    private function lt(mixed $val1, mixed $val2): bool {
+        return $val1 < $val2;
+    }
+
+    private function gt(mixed $val1, mixed $val2): bool {
+        return $val1 > $val2;
+    }
+
+    private function eq(mixed $val1, mixed $val2): bool {
+        return $val1 === $val2;
     }
 }
